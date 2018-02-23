5113c8e0ddc8e4dc12ca12554ff06cc6ee741cb4
Kaitlyn <kgraf1@ycp.edu>
2018-02-23 11:24:38 -0500

    Changed the size of the square from a 40x40 to a 95x95.

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
@@ -85,8 +85,8 @@ public class GameView extends JPanel {
 				Square square = new Square();
 				square.setX(300.0);
 				square.setY(220.0);
-				square.setWidth(40.0);
-				square.setHeight(40.0);
+				square.setWidth(95.0);
+				square.setHeight(95.0);
 				model.setSquare(square);
 				
 				GameController controller = new GameController();
